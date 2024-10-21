64ee578cda39734c0da13a265de287beb287f65b
mvdiani <ggrt91@gmail.com>
2024-10-21 09:55:03 +0200

    eslint and fix /user

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,25 +7,33 @@ const prisma = new PrismaClient();
 app.use(express.json());
 
 app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
+  try {
+    const users = await prisma.user.findFirst();
     res.json(users);
+    console.log(users);
+  }
+  catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 app.post('/users', async (req, res) => {
-    const { name, email } = req.body;
-    try {
-        const user = await prisma.user.create({
-            data: {
-                name,
-                email,
-            },
-        });
-        res.json(user);
-    } catch (error) {
-        res.status(400).json({ error: 'User already exists' });
-    }
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({ error: 'User already exists' });
+  }
 });
 
 app.listen(3001, () => {
-    console.log('Server running on http://localhost:3001');
+  console.log('Server running on http://localhost:3001');
 });
