dd86b47b2fa4d6866ea31c87290a8d2bdef5faa6
Medghali majid <majid.medghali@bordeaux-inp.fr>
2024-11-02 16:24:29 +0100

    Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,21 +5,21 @@ import logoEtuderecup from '/logoEtudeRecup.jpg';
 
 const Header = (onUploadClick) => {
     return (
-<header className={styles.header}>
-    <div className={styles.headerImage}>
-        <img src={logoEtuderecup} className="logo" alt="logo_etude_recup" />
-    </div>
-    <div className={styles.headerButtons}>
-    <button className={styles.button} onClick={onUploadClick}>Importer un fichier</button>
+    <header className={styles.header}>
+        <div className={styles.headerImage}>
+            <img src={logoEtuderecup} className="logo" alt="logo_etude_recup" />
+        </div>
+        <div className={styles.headerButtons}>
+        <button className={styles.button} onClick={onUploadClick}>Importer un fichier</button>
 
-        <button className={styles.button} >
-            Contact
-        </button>
-        <button className={styles.button}>
-            Déconnexion
-        </button>
+            <button className={styles.button} >
+                Contact
+            </button>
+            <button className={styles.button}>
+                Déconnexion
+            </button>
 
-    </div>
+        </div>
     </header>
     )
 }
